8a8a012c5132604cbd64c8d73126d5680eaf0058
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-03 00:50:30 +0600

    Update distance_of_node_using_bfs.cpp

--- Graph/BFS/distance_of_node_using_bfs.cpp
+++ Graph/BFS/distance_of_node_using_bfs.cpp
@@ -1,1 +1,24 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>adj_node[100];
+bool complited[100];
+int level[100];
+
+
+
+
+int main(){
+    int nodes,edges;
+    cin>>nodes>>edges;
+    while(edges--){
+        int x,y;
+        cin>>x>>y;
+        adj_node[x].push_back(y);
+        adj_node[y].push_back(x);
+    }
+    bfs(1);
+    cout<<level[4]<<endl;
+    return 0;
+}
