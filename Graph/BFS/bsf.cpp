338884e231cabc45c92829ffa35955134df2cef5
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-02 23:46:48 +0600

    Create bsf.cpp

---
+++ Graph/BFS/bsf.cpp
@@ -0,0 +1,9 @@
+/* 
+WHY BFS IS USED?
+1. For normal level order traversing.
+2. Finding shortest path for unweighted graph.
+
+we use Bfs when there are high probability to find ans by visiting shortest path.
+
+
+*/
