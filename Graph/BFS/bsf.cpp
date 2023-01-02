2b6bc2afde373808beeac8c5b2f9e410d378dd56
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-02 23:47:39 +0600

    Update bsf.cpp

--- Graph/BFS/bsf.cpp
+++ Graph/BFS/bsf.cpp
@@ -7,5 +7,5 @@ we use Bfs when there are high probability to find ans by visiting shortest path
 for example, 
 we need a bike and we know that the batter place for buying bike is sylhet border and comilla border. Our location is dhaka, in such a case, we know that comilla is nearest place to achive bike, and the cost will be less. so we go to comilla. In such type of situation we use BFS.
 
-
+on the otherhand, we do not the actual place of finding better bike in banglades. and we want bike in mimimum cost and time, in such a situation we use DFS. whcich is depth first search.if we go by level order distance from dhaka, if the place is far then the time and cost will be high. so BFS will not the best option.
 */
