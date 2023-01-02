65d7a123e005169940f16d71feec32a6d30cb4a0
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-02 23:48:26 +0600

    Update bsf.cpp

--- Graph/BFS/bsf.cpp
+++ Graph/BFS/bsf.cpp
@@ -9,3 +9,24 @@ we need a bike and we know that the batter place for buying bike is sylhet borde
 
 on the otherhand, we do not the actual place of finding better bike in banglades. and we want bike in mimimum cost and time, in such a situation we use DFS. whcich is depth first search.if we go by level order distance from dhaka, if the place is far then the time and cost will be high. so BFS will not the best option.
 */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>adj_node[100];
+bool complited[100];
+
+
+int main(){
+    int nodes,edges;
+    cin>>nodes>>edges;
+    while(edges--){
+        int x,y;
+        cin>>x>>y;
+        adj_node[x].push_back(y);
+        adj_node[y].push_back(x);
+    }
+    bfs(1);
+    return 0;
+}
