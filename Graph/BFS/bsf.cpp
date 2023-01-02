0e4c5411b2a9ff2981a451d7050ef7af1ca9f7c3
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-02 23:47:18 +0600

    Update bsf.cpp

--- Graph/BFS/bsf.cpp
+++ Graph/BFS/bsf.cpp
@@ -4,6 +4,8 @@ WHY BFS IS USED?
 2. Finding shortest path for unweighted graph.
 
 we use Bfs when there are high probability to find ans by visiting shortest path.
+for example, 
+we need a bike and we know that the batter place for buying bike is sylhet border and comilla border. Our location is dhaka, in such a case, we know that comilla is nearest place to achive bike, and the cost will be less. so we go to comilla. In such type of situation we use BFS.
 
 
 */
