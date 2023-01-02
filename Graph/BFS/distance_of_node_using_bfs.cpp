576f5605dc4030a46a09434d806d3198bb5908de
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-03 00:50:39 +0600

    Update distance_of_node_using_bfs.cpp

--- Graph/BFS/distance_of_node_using_bfs.cpp
+++ Graph/BFS/distance_of_node_using_bfs.cpp
@@ -7,7 +7,33 @@ bool complited[100];
 int level[100];
 
 
+void bfs(int source){
+    memset(complited,false,sizeof(complited));
+    memset(level,-1,sizeof(level));
 
+    queue<int>q;
+    q.push(source);
+    level[source] = 0;
+
+    while(!q.empty()){
+        int parent = q.front();
+        q.pop();
+        if(complited[parent] == false){
+            // cout<<parent<<endl;
+            complited[parent] = true;
+        }
+        
+        for(int i = 0; i < adj_node[parent].size();i++){
+            int children = adj_node[parent][i];
+            if(complited[children] == false){
+                q.push(children);
+                level[children] = level[parent]+1;
+            }
+            
+        }
+    }
+
+}
 
 int main(){
     int nodes,edges;
