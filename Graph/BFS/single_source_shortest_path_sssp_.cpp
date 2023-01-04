29411e9cfddb7371e97e4037527866700c91e910
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-04 18:27:53 +0600

    Update single_source_shortest_path_sssp_.cpp

--- Graph/BFS/single_source_shortest_path_sssp_.cpp
+++ Graph/BFS/single_source_shortest_path_sssp_.cpp
@@ -1,1 +1,25 @@
 
+/* 
+0---1---2----3
+    |   |
+    |   |
+    5---4
+
+Sample Input:
+6 6 // nodes = 6 and edges = 6
+
+0 1
+1 2
+2 3
+1 5
+2 4
+5 4
+
+OUTPUT:
+Level of 0: 0
+Level of 1: 1
+Level of 2: 2
+Level of 3: 3
+Level of 4: 3
+Level of 5: 2
+ */
