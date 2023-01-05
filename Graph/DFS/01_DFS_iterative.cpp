9f4e2fc809c9499f197f220c1550059c9d11a9cb
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-05 08:54:25 +0600

    Update 01_DFS_iterative.cpp

--- Graph/DFS/01_DFS_iterative.cpp
+++ Graph/DFS/01_DFS_iterative.cpp
@@ -9,3 +9,27 @@ input:
 
 output: 0 2 1 4 3 
  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5; // 1e5 == 10^5 
+vector<int>adj_node[N]; // for storing adjecnt list of a node..
+int visited[N];
+
+
+int main(){
+    int nodes,edges;
+    cin>>nodes>>edges;
+    for(int i = 0; i<edges;i++){
+        int x,y;
+        cin>>x>>y;
+        adj_node[x].push_back(y);
+        adj_node[y].push_back(x);
+        
+    }
+    dfs(1);
+
+    return 0;
+}
+
