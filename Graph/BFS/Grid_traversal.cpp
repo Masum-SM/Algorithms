4bb063e28af3b8c9c7f481493699d6db7087fc30
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-06 22:16:10 +0600

    Update Grid_traversal.cpp

--- Graph/BFS/Grid_traversal.cpp
+++ Graph/BFS/Grid_traversal.cpp
@@ -17,3 +17,29 @@ using namespace std;
 int n,m;
 const int N = 2002;
 int maze[N][N], visited[N][N], level[N][N];
+
+int main(){
+    cin>>n>>m;
+    pair<int,int>src,dst;
+
+    memset(level,-1,sizeof(level));
+
+    for(int i = 0 ; i < n ; i++){
+        string input;
+        cin>>input;
+        for(int j = 0 ; j < m ; j++){
+            if(input[j] == '#'){
+                maze[i][j] = -1;
+            }
+            else if(input[j] == 'A'){
+                src = {i,j};
+            }
+            else if(input[j] == 'B'){
+                dst = {i,j};
+            }
+        }
+    }
+
+
+    return 0;
+}
