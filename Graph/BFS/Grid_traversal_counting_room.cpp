6df96c635c031d98681a4c1a1f39c3b6413f5397
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-07 18:39:47 +0600

    Update Grid_traversal_counting_room.cpp

--- Graph/BFS/Grid_traversal_counting_room.cpp
+++ Graph/BFS/Grid_traversal_counting_room.cpp
@@ -31,3 +31,30 @@ Input:
 Output:
 3
  */
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m;
+const int N = 2002;
+int maze[N][N], visited[N][N];
+
+
+int main(){
+    
+    cin>>n>>m;
+    for(int i = 0 ; i < n ; i++){
+        string input;
+        cin>>input;
+        for(int j = 0; j<m; j++){
+            if(input[j] == '#'){
+                maze[i][j] = -1;
+            }
+        }   
+    }
+
+
+    return 0;
+}
