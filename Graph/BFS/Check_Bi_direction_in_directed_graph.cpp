0dd993cae68c0bcf4950842bad282879dcda5e12
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-12 23:29:09 +0600

    take input and make vector for adjecent_list

--- Graph/BFS/Check_Bi_direction_in_directed_graph.cpp
+++ Graph/BFS/Check_Bi_direction_in_directed_graph.cpp
@@ -1,10 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5;
+vector<int>adj_node[N];
+int visited[N];
 
 
 
 int main(){
+    int n,e;
+    cin>>n>>e;
+    while(e--){
+        int x,y;
+        cin>>x>>y;
+        adj_node[x].push_back(y);
+    }
+    if(!bfs(1)){
+        cout<<"The graph is not bi-directional"<<endl;
+    }
+    else{
+        cout<<"The graph is bi-directional"<<endl;
+    }
 
 
     return 0;
