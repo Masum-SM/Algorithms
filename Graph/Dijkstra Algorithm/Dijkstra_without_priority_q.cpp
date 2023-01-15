387cb6588a3c476832efe44638ce42c7c207ecce
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-15 23:55:25 +0600

    Dijkstra function

--- Graph/Dijkstra Algorithm/Dijkstra_without_priority_q.cpp
+++ Graph/Dijkstra Algorithm/Dijkstra_without_priority_q.cpp
@@ -7,6 +7,22 @@ const int INF = 1e9;
 int nodes, edges;
 vector<pair<int, int>> adj_list[N];
 
+int d[N], visited[N];
+
+void dijkstra(int src)
+{
+    for (int i = 1; i <= nodes; i++)
+    {
+        d[i] = INF;
+    }
+
+    d[src] = 0;
+
+
+    
+    
+    }
+}
 
 
 int main()
@@ -20,7 +36,8 @@ int main()
         adj_list[x].push_back({y, w});
         adj_list[y].push_back({x, w});
     }
-  
+     int src = 1;
+    dijkstra(src);
   
       return 0;
 }
